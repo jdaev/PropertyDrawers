0b411d65b77ca8f9d82eca27756098c6b203569b
Javed Hussain <jdhn42@gmail.com>
2020-12-17 21:06:30 -0500

    Initial commit

---
+++ Assets/Editor/PropertyExample.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomPropertyDrawer(typeof(TestScript.Togglable<>))]
+    public class PropertyExample : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+            
+            var indent = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+            SerializedProperty visibilityData = property.FindPropertyRelative("visible");
+            SerializedProperty valueData = property.FindPropertyRelative("value");
+            
+            Rect visibilityRect = new Rect(position.x, position.y, 50, position.height);
+            Rect labelRect = new Rect(position.x + 95, position.y, 50, position.height);
+            Rect valueRect = new Rect(position.x + 35, position.y, 30, position.height);
+            EditorGUI.PropertyField(visibilityRect, property.FindPropertyRelative("visible"),
+                GUIContent.none);
+            EditorGUI.LabelField(labelRect, label);
+            if (visibilityData.boolValue)
+            {
+                EditorGUI.PropertyField(valueRect, property.FindPropertyRelative("value"), GUIContent.none);
+            }
+
+            EditorGUI.indentLevel = indent;
+
+            EditorGUI.EndProperty();
+        }
+    }
+}
---
+++ Assets/TestScript.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class TestScript : MonoBehaviour
+{
+    public FloatEvent floatEvent;
+    public Vector3Event vector3Event;
+    
+    public AnimationCurve animationCurve;
+    public float animationDuration = 2;
+    
+    public Togglable<int> togglableInt = new Togglable<int>(5,true);
+    public Togglable<string> togglableString= new Togglable<string>("Test",true);
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        StartCoroutine(ButtonScaleTransition());
+    }
+
+    public IEnumerator ButtonScaleTransition()
+    {
+        float timeElapsed = 0;
+        while (timeElapsed<animationDuration)
+        {
+            timeElapsed += Time.deltaTime;
+            float y = animationCurve.Evaluate(Mathf.Clamp01(timeElapsed / animationDuration));
+            floatEvent.Invoke(y);
+            vector3Event.Invoke(Vector3.one*y);
+            yield return null;
+        }
+
+    }
+    
+    [System.Serializable]
+    
+    public class FloatEvent : UnityEvent<float>
+    {
+        
+    }
+    
+    [System.Serializable]
+    
+    public class Vector3Event : UnityEvent<Vector3>
+    {
+        
+    }
+    
+    [System.Serializable]
+    public class Togglable<T>
+    {
+        public T value;
+        public bool visible;
+
+        public Togglable(T value, bool visible)
+        {
+            this.value = value;
+            this.visible = visible;
+        }
+    }
+}
+
